5c0829c70582d15fab1b7d1e25e951216af0933a
Ben Pickering <benpicko1@live.co.uk>
2017-05-09 15:09:57 +0100

    Updates to title and pic changer

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+var myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+  var mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/mozilla-firefox.png') {
+    myImage.setAttribute('src', 'images/aa5e4548975eb164dd04de4591683ba4.png');
+  } else {
+    myImage.setAttribute('src', 'images/mozilla-firefox.png');
+  }
+};
